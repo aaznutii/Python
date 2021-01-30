35b07f4657b0769da1290f81725c80562e6771d7
Aaznutii <aaznutii@mail.ru>
2021-01-30 21:56:46 +0400

    lessons Заданине 3

--- lessonsPD/lessons_5/less_3.py
+++ lessonsPD/lessons_5/less_3.py
@@ -6,3 +6,36 @@
 Иванов 23543.12
 Петров 13749.32
 """
+
+
+def get_workers():
+    """
+    Запись данных из файла в словарь
+    :return: {'Скриптов': 100000,..}
+    """
+    with open("for_less_3.txt", 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    workers = {}
+    for line in lines:
+        try:
+            name, money = line.strip('\n').split(' ')
+            workers.update({name: int(money)})
+        except ValueError:
+            continue
+    return workers
+
+
+def get_needy():
+    workers = get_workers()
+    needing = [name for name in workers if workers.get(name) < 20000]
+    print(f'Получают заработную плату менее 20 000: {needing}')
+
+
+def get_average():
+    workers = get_workers()
+    average = sum(workers.values()) / len(workers)
+    print(f'Средняя величина дохода сотрудников :{average}')
+
+
+get_needy()
+get_average()
