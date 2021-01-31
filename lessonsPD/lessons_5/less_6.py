93ec52c06841c7944f822612229b9d287dc536a5
Aaznutii <aaznutii@mail.ru>
2021-01-31 17:01:26 +0400

    lessons Заданине 6 Промежуточный коммит

--- lessonsPD/lessons_5/less_6.py
+++ lessonsPD/lessons_5/less_6.py
@@ -8,3 +8,29 @@
                                         Физкультура:   —   30(пр)   —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+
+
+def get_dict():
+    """
+    Создание словаря с исходными данными и суммой часов
+    :return: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
+    """
+    with open('for_less_6.txt', 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    disciplines = {}
+    for line in lines:
+        try:
+            name, subjects_numbers = line.strip('\n').split(':')
+            subjects_numbers = [list(subjects) for subjects in subjects_numbers.split(' ') if subjects != '']
+
+        except ValueError:
+            continue
+        print(subjects_numbers)
+    # for subjects in disciplines.values():
+    #     print(subjects)
+    #     # hours = [list(el) for el in subjects]
+    #     # print(hours)
+    # print(disciplines)
+
+
+get_dict()
