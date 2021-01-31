7e7c84893fa31949ba7e6c38eaa6fc45c66ff028
Aaznutii <aaznutii@mail.ru>
2021-01-31 15:09:49 +0400

    lessons Заданине 5 Выполнено

--- lessonsPD/lessons_5/less_5.py
+++ lessonsPD/lessons_5/less_5.py
@@ -4,10 +4,10 @@
 """
 
 
-def open_file(file_name, mode, values=None):
+def open_file(file_name='for_less_5.txt', mode='r', values=None):
     """
     Открывет файл для записи/создания или чтения в зависимости от наличия значений для записи
-    :param file_name: 'for_less_4.txt'
+    :param file_name: 'for_less_5.txt'
     :param mode: 'r'
     :param values: 'values'
     """
@@ -19,23 +19,34 @@ def open_file(file_name, mode, values=None):
             file.write(values)
 
 
-def input_number():
+def write_input_number():
     """
-    Ввод цифр через пробел, их запись в файл до момента ввода "!"
+    Ввод цифр через пробел, их запись в файл до момента пустого ввода "Enter". Файл перезаписывается.
+    Проводится проверка, что введены цифры.
     """
-    print('Введите цифры, разделенные пробелами. Цифры будут записаны в файл.'
-          'Для прекращения ввода введите любую букву')
+    print('Введите цифры. Цифры будут записаны в файл.'
+          'Для прекращения ввода нажмите Ввод')
     var_dict = {'user_numbers': [], 'stop': 0}
     while var_dict['stop'] == 0:
         user_values = input().split(' ')
         for el in user_values:
             if el.isnumeric():
-                var_dict['user_numbers'].append(int(el))
+                var_dict['user_numbers'].append(el + ' ')
             else:
                 var_dict['stop'] = 1
                 break
-        print('Введенны числа и записаны в файл: {}'.format(sum(var_dict['user_numbers'])))
+    open_file("for_less_5.txt", 'w', ''.join(var_dict['user_numbers']))
+    print('Введенны числа и записаны в файл: {}'.format(''.join(var_dict['user_numbers'])))
 
 
-# Запуск Задание 5
-input_number()
+def sum_numbers_from_file():
+    """
+    Суммирование цифр из файла, вызов функци добавления цифр и записи в файл.
+    """
+    write_input_number()
+    numbers = ' '.join(open_file('for_less_5.txt', 'r')).split()
+    sum_numbers = sum([int(el) for el in numbers])
+    print(f'Итого сумма цифр в файле: {sum_numbers}')
+
+
+sum_numbers_from_file()
