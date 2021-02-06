6f95fbbec872d11d3b17e0fb927e3549af1226a4
Aaznutii <aaznutii@mail.ru>
2021-02-06 20:59:38 +0400

    lessons 6 1, 2, 3, 4 Выполнены

--- lessonsPD/lessons_6/less_4.py
+++ lessonsPD/lessons_6/less_4.py
@@ -16,43 +16,88 @@ class Car:
     speed = 0
     color = ''
     name = ''
-    is_police = bool
+    is_police = False
+    count = 0
+
+    def __init__(self, speed, color, name, turn, is_police=False):
+        self.count += 1
+        self.speed = float(speed)
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+        self.get_status(turn)
+        print('----------------------')
+
+    def get_status(self, turn=None):  # Возможно усложняю, сделал для упорядочевания инит
+        if self.speed == 0:
+            self.stop()
+        else:
+            if turn is None:
+                self.go()
+            else:
+                self.turn(turn)
 
     def go(self):
-        if self.speed > 0:
-            print('Автомобиль поехал')
-            self.show_speed()
+        print('Автомобиль поехал прямо')
+        self.show_speed()
 
     def stop(self):
         if self.speed == 0:
             print('Автомобиль остановился')
 
-    def turn(self):
-        print('Автомобиль певернул налево')
-        print('Автомобиль певернул направо')
-        print('Автомобиль развернулся')
+    def turn(self, turn):
+        turns = ['налево', 'на лево', 'лево', 0, 'left']
+        code = ('Автомобиль повернул налево', 'Автомобиль повернул направо')[turn not in turns]
+        print(code)
+        self.show_speed()
 
     def show_speed(self):
-        print(f'Скорость автомобиля {self.speed}')
+        speed = self.speed
+        print(f'Скорость автомобиля {speed}')
+        if self.is_police is True:
+            print('Полицейская машина')
 
 
 class TownCar(Car):
-    super().is_police = False
+    def __init__(self, speed, color, name, turn=None):
+        print(f'TownCar: {name}, {color}')
+        super().__init__(speed, color, name, turn, is_police=False)
 
     def show_speed(self):
         if self.speed > 60:
             print('Превышение скорости')
+            print(f'Скорость автомобиля {self.speed}')
+        else:
+            print(f'Скорость автомобиля {self.speed}')
 
 
 class SportCar(Car):
-    super().is_police = False
+    def __init__(self, speed, color, name, turn=None):
+        print(f'SportCar: {name}, {color}')
+        super().__init__(speed, color, name, turn, is_police=False)
+
 
 class WorkCar(Car):
-    super().is_police = False
+    def __init__(self, speed, color, name, turn=None):
+        print(f'WorkCar: {name}, {color}')
+        super().__init__(speed, color, name, turn, is_police=False)
+
     def show_speed(self):
         if self.speed > 40:
             print('Превышение скорости')
+            print(f'Скорость автомобиля {self.speed}')
+        else:
+            print(f'Скорость автомобиля {self.speed}')
 
 
 class PoliceCar(Car):
-    super().is_police = True
+    def __init__(self, speed, color, name, turn=None):
+        print(f'PoliceCar: {name}, {color}')
+        super().__init__(speed, color, name, turn, is_police=True)
+
+
+test = TownCar(1, 'red', 'Lexus')
+test1 = SportCar(70, 'dlu', 'Jaguar', 'Право')
+test2 = WorkCar(60, 'green', 'Камаз', 'лево')
+test3 = PoliceCar(10, 'Red', 'Lexus', 0)
+test4 = TownCar(100, 'Red', 'Lexus', 1)
