dec8bbfefce2bc46c3ee69055b6f432724a0b60f
Aaznutii <aaznutii@mail.ru>
2021-02-08 16:40:10 +0400

    lessons 7 1, 2 Процесс

--- lessonsPD/lessons_7/less_2.py
+++ lessonsPD/lessons_7/less_2.py
@@ -14,31 +14,60 @@ from abc import ABC
 class Dress(ABC):
     size = 0
 
-    def __init__(self, size):
-        self.size = size
-
-    # def amount_of_fabric(self, v=None, h=None):
-    #     if v is None and h == None:
-    #         result = 'Вы не указли размеров одежды для расчета'
-    #     elif v is None:
-    #         result = 2*int(h) + 0.3
-    #     else:
-    #         result = int(v)/6.5 + 0.5
-
 
 class Coat(Dress):
-    def __init__(self, size_v):
-        super().__init__(self.size)
-        self.size = size_v
-        self.amount_of_fabric(size_v)
-
-    def amount_of_fabric(self, size_v):
-        result = int(size_v) / 6.5 + 0.5
+    def amount_of_fabric(self, size):
+        result = int(size) / 6.5 + 0.5
         return result
 
 
-# class Suit(Dress, h):
-#     result = 2 * int(h) + 0.3
+class Suit(Dress):
+    def amount_of_fabric(self, size):
+        result = 2 * int(size) + 0.3
+        return result
 
-c = Coat(50)
+
+c = Coat().amount_of_fabric(50)
 print(c)
+c1 = Coat().amount_of_fabric(70)
+print(c1)
+print(c + c1)
+
+s = Suit().amount_of_fabric(50)
+s2 = Suit().amount_of_fabric(30)
+print(s)
+print(s2)
+print(s + s2)
+print(c + c1 + s + s2)
+
+"""
+# класс Auto
+class Auto:
+
+    # конструктор класса Auto
+    def __init__(self, year):
+        # Инициализация свойств.
+        self.year = year
+
+    # создаем свойство года
+    @property
+    def year(self):
+        return self.__year
+
+    # сеттер для создания свойств
+    @year.setter
+    def year(self, year):
+        if year < 2000:
+            self.__year = 2000
+        elif year > 2019:
+            self.__year = 2019
+        else:
+            self.__year = year
+
+    def get_auto_year(self):
+        return f"Автомобиль выпущен в {str(self.year)} году"
+
+a = Auto(2090)
+print(a.get_auto_year())
+
+"""
