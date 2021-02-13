65c4eb4d43063650d6a5d5207047ef9794caca5e
Aaznutii <aaznutii@mail.ru>
2021-02-13 15:52:19 +0400

    lessons 8 1, 2 выполнено

--- lessonsPD/lessons_8/less_1.py
+++ lessonsPD/lessons_8/less_1.py
@@ -4,7 +4,7 @@
 преобразовывать их тип к типу «Число». Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и
 года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
-
+# Возможно не до конц понял задание. Пока сделал так.
 
 class Date:
 
@@ -23,20 +23,24 @@ class Date:
         except ValueError:
             print('Ошибка. Введен текст.\n'
                   'Введите корректно дату в формате dd-mm-yyyy')
+            return False
 
     @staticmethod
     def validation_int(list):
-        default = (['day', 31], ['month', 12], ['years', 3000])
-        result = []
-        for i, el in enumerate(list):
-            if el == 0 or el > default[i][1]:
-                print(f'Некорректно укзана дата. Исправьте {default[i][0]}')
-                break
-            else:
-                result.append(str(el))
-        if len(result) == 3:
-            result = ':'.join(result)
-            print(f'Введена дата: {result}')
+        if list is False:
+            return
+        else:
+            default = (['day', 31], ['month', 12], ['years', 3000])
+            result = []
+            for i, el in enumerate(list):
+                if el == 0 or el > default[i][1]:
+                    print(f'Некорректно укзана дата. Исправьте {default[i][0]}')
+                    break
+                else:
+                    result.append(str(el))
+            if len(result) == 3:
+                result = ':'.join(result)
+                print(f'Введена дата: {result}')
 
 
 d = Date('12-12-2021')
--- lessonsPD/lessons_8/less_2.py
+++ lessonsPD/lessons_8/less_2.py
@@ -3,3 +3,21 @@
 вводимых пользователем. При вводе пользователем нуля в качестве делителя программа должна корректно обработать эту
 ситуацию и не завершиться с ошибкой.
 """
+
+
+class ZeroExc(Exception):
+    def __init__(self, number1, number2):
+        self.get_status_division(number1, number2)
+
+    def get_status_division(self, number1, number2):
+        try:
+            result = number1 / number2
+            print(result)
+        except ZeroDivisionError:
+            print('Деление на ноль')
+
+
+z = ZeroExc(5, 0)
+z1 = ZeroExc(5, 1)
+z2 = ZeroExc(-2, 0)
+z3 = ZeroExc(5, 15)
