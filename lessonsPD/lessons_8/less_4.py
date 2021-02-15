c25fe8c4a1d2e8ae1243cebb9f1b04e50ac4850b
Aaznutii <aaznutii@mail.ru>
2021-02-15 15:37:35 +0400

    lessons 8 1, 2, 3(1), 4 process выполнено

--- lessonsPD/lessons_8/less_4.py
+++ lessonsPD/lessons_8/less_4.py
@@ -4,3 +4,38 @@
 В базовом классе определить параметры, общие для приведенных типов. В классах-наследниках реализовать параметры,
 уникальные для каждого типа оргтехники.
 """
+
+
+class Warehouse:
+    pass
+
+
+class OfficeEquipment(Warehouse):
+    name = ''
+    color = ''
+    count = 0
+    weight = 0
+    size = [0, 0, 0]
+    cost = 0
+
+
+class Printer(OfficeEquipment):
+    def __init__(self, name, color, count, weight, size, cost, print_speed):
+        self.name = name
+        self.color = color
+        self.count = count
+        self.weight = weight
+        self.size = size
+        self.cost = cost
+        self.print_speed = print_speed
+
+
+class Scaner(OfficeEquipment):
+    pass
+
+
+class Copier(OfficeEquipment):
+    pass
+
+
+p = Printer()
