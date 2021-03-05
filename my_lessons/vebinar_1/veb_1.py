4d70405c162f40ff355279a99f4294c53d34e4c7
Aaznutii <aaznutii@mail.ru>
2021-03-05 23:54:18 +0400

    Вебинар Просто для памяти по результатам просмотра видеоуроков.

--- my_lessons/vebinar_1/veb_1.py
+++ my_lessons/vebinar_1/veb_1.py
@@ -23,3 +23,13 @@ for el in nums:
 print(sum, perf_counter() - start)
 
 print(id(sum))  # Идентификатор в памяти
+
+# map, filter, zip = итераторы
+lt = ['dfdf', 'fgfggf', 'ghghh']
+res = map(str.title, lt)  # экономично по памяти
+print(res)
+print(*res)  # * для распаковки
+print(*res)  # Не напечатает, потому что итератор исчерпан
+
+print(tuple(map(str.title, lt)))  # чуть менее затратно, поскольку не изменяемый тип данных
+print(list(map(str.title, lt)))  # Очень затратно по памяти, поскольку создает список
